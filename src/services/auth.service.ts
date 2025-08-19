df3fb4f0fe96c80782d91dc1fa5cf346ed9108a7
Yam Sol Britto Bertamini <yam.sol@inatel.br>
2025-08-18 21:04:37 -0300

    feat: Conflito forcado parte 02

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -20,6 +20,7 @@ export class AuthService {
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
+    console.log('Password', password); // Debugging line
     if (!passwordMatch) {
       return null;
     }
